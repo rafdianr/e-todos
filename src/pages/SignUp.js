8c38d5ae4c92cdf98b931f8dfbaf5d9255825e24
rafdianr <rafdian.ramadhan@gmail.com>
2020-05-24 17:49:55 +0700

    [Rafd] - edit sign page

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -13,9 +13,23 @@ function SignUp() {
               <p className="sign-left-item-des">
                 To keep connected with us please login with your personal info
               </p>
+              <button class="reg-btn white">SIGN IN</button>
             </div>
           </div>
-          <div className="sign-right"></div>
+          <div className="sign-right">
+            <div className="sign-right-item">
+              <h1 className="sign-right-item-title">Created Account</h1>
+              <p className="sign-right-item-des">
+                or use email for registration
+              </p>
+              <form className="sign-form" action="submit">
+                <input type="text" placeholder="Name" />
+                <input type="text" placeholder="Email" />
+                <input type="text" placeholder="Password" />
+                <button class="reg-btn">SIGN UP</button>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     </section>
