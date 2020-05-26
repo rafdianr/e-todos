4f49421a5afe6375d0c65efb233554bb39c530d7
rafdianr <rafdian.ramadhan@gmail.com>
2020-05-26 22:19:29 +0700

    [Rafd] - edit code

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -2,12 +2,14 @@ import React, { Fragment } from "react";
 import SignUp from "../pages/SignUp";
 import SignIn from "../pages/SignIn";
 import { Route } from "react-router-dom";
+import TaskDashboard from "../pages/TaskDashboard";
 
 function Routes() {
   return (
     <Fragment>
       <Route path="/" component={SignUp} exact />
       <Route path="/signin" component={SignIn} exact />
+      <TaskDashboard />
     </Fragment>
   );
 }
