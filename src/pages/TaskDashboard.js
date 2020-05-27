7336be4ec15ead597f7683786ade2b0d24d51c0b
rafdianr <rafdian.ramadhan@gmail.com>
2020-05-27 20:44:29 +0700

    [Rafd] - edit

--- src/pages/TaskDashboard.js
+++ src/pages/TaskDashboard.js
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import TaskCards from "../components/TaskCards";
 
-function TaskDashboard() {
+const TaskDashboard = () => {
   return (
     <section className="sign">
       <div className="container dashboard">
@@ -47,6 +47,6 @@ function TaskDashboard() {
       </div>
     </section>
   );
-}
+};
 
 export default TaskDashboard;
