7209683b6a44d8788db9985b5284cb28bc9e6073
rafdianr <rafdian.ramadhan@gmail.com>
2020-06-02 20:54:39 +0700

    [Rafd] - add sign up function with api

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,9 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../assets/style/SignPage.css";
 import { Link } from "react-router-dom";
 import SocialIcon from "../components/SocialIcon";
+import axios from "axios";
 
-function SignUp() {
+const SignUp = (props) => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    let token = localStorage.getItem("token");
+    if (token) props.history.replace("/");
+  }, [props.history]);
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+    console.log("fungsi signup");
+    axios({
+      method: "POST",
+      url: "https://mini-project1.herokuapp.com/api/v1/user/register",
+      data: {
+        name,
+        email,
+        password,
+      },
+    })
+      .then((res) => {
+        console.log(res);
+        if (res.data.status) {
+          localStorage.setItem("token", res.data.data.token);
+          setIsLoading(false);
+          setName("");
+          setEmail("");
+          setPassword("");
+          props.history.replace("/");
+        } else {
+          //handle error
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        setIsLoading(false);
+        setName("");
+        setEmail("");
+        setPassword("");
+      });
+  };
+
   return (
     <section className="sign">
       <div className="container">
@@ -27,10 +72,29 @@ function SignUp() {
               <p className="sign-right-item-des">
                 or use email for registration
               </p>
-              <form className="sign-form" action="submit">
-                <input type="text" placeholder="Name" />
-                <input type="text" placeholder="Email" />
-                <input type="password" placeholder="Password" />
+              <form
+                className="sign-form"
+                action="submit"
+                onSubmit={handleSignup}
+              >
+                <input
+                  type="text"
+                  placeholder="Name"
+                  value={name}
+                  onChange={(event) => setName(event.target.value)}
+                />
+                <input
+                  type="text"
+                  placeholder="Email"
+                  value={email}
+                  onChange={(event) => setEmail(event.target.value)}
+                />
+                <input
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(event) => setPassword(event.target.value)}
+                />
                 <button className="reg-btn">SIGN UP</button>
               </form>
             </div>
@@ -39,6 +103,6 @@ function SignUp() {
       </div>
     </section>
   );
-}
+};
 
 export default SignUp;
